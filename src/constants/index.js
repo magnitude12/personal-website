7547a6b63a6e3b9561b59c0d21369622b208c6d8
magnitude12 <magnitude12i@gmail.com>
2023-04-30 17:40:50 +0545

    index.js update

---
+++ src/constants/index.js
@@ -0,0 +1,266 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Lorem Ipsum",
+      company_name: "Lorem",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "Lorem",
+      points: [
+        "Adipisicing mollit magna eiusmod eu.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      ],
+    },
+    {
+      title: "Lorem",
+      company_name: "Ipsum",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Lorem Ipsum",
+      points: [
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      ],
+    },
+    {
+      title: "Lorem",
+      company_name: "Ipsum",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Lorem Ipsum",
+      points: [
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      ],
+    },
+    {
+      title: "Lorem Ipsum",
+      company_name: "Lorem",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Lorem Ipsum",
+      points: [
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      name: "Lorem Ipsum",
+      designation: "Lorem",
+      company: "Lorem Ipsum",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      name: "Lorem Ipsum",
+      designation: "Lorem",
+      company: "Lorem Ipsum",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      name: "Lorem Ipsum",
+      designation: "Lorem",
+      company: "Lorem Ipsum",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Lorem Ipsum",
+      description:
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Lorem Ipsum",
+      description:
+        "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Lorem Ipsum",
+      description:
+      "Proident occaecat consectetur velit tempor aliqua minim pariatur voluptate adipisicing ut pariatur.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
