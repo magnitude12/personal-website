b5e970e7a75e8ba0a2c5c6fd192f394a9c816e27
magnitude12 <magnitude12i@gmail.com>
2023-05-26 01:01:20 +0545

    removing 3d balls, too resource heavy

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,57 +1,19 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+import React from 'react'
+import Tilt from "react-parallax-tilt";
 
-import CanvasLoader from "../Loader";
-
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
-
+const BallCanvas = ({icon,name}) => {
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
-  );
-};
+    <Tilt
+    options={{
+      max: 70,
+      scale: 1,
+      speed: 500,
+    }}
+    className='w-[100px] h-[100px] rounded-full bg-[#333333bb] p-4 ball_card'
+  >
+      <img src={icon} alt="tech Icons" title={name} className="w-full h-full object-cover " />
+    </Tilt>
+  )
+}
 
-const BallCanvas = ({ icon }) => {
-  return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
-
-      <Preload all />
-    </Canvas>
-  );
-};
-
-export default BallCanvas;
+export default BallCanvas
