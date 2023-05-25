f70f51a208286ac0b4f1706c559ee850154ad1d2
magnitude12 <magnitude12i@gmail.com>
2023-05-26 01:29:43 +0545

    removing stars fom contact

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
+        <Contact />
       </div>
     </BrowserRouter>
   );
