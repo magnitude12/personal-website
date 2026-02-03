b89fe1462045916e6c101631cb4c78d5eeba2ee1
magnitude12 <113277446+magnitude12@users.noreply.github.com>
2026-02-03 01:36:55 -0600

    update contact fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,11 +38,13 @@ const Contact = () => {
         formRef.current, // Pass the form ref here
         {
           publicKey: import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
-        }
+        
+        },
       )
       .then(
         () => {
           setLoading(false);
+          console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
           alert("Thanks for reaching out, I'll get back to you asap!");
 
           setForm({
@@ -55,15 +57,17 @@ const Contact = () => {
           setLoading(false);
           console.error(error);
           alert("Damn, an error. Mind giving another go?");
-        }
+        },
       );
   };
 
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -71,48 +75,48 @@ const Contact = () => {
         <form
           ref={formRef} // This is the bridge to emailjs.sendForm
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className="mt-12 flex flex-col gap-8"
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
             <input
-              type='text'
-              name='name' // Ensure this matches your EmailJS template variable {{name}}
+              type="text"
+              name="name" // Ensure this matches your EmailJS template variable {{name}}
               value={form.name}
               onChange={handleChange}
               placeholder="Give me your name please"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
               required
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
             <input
-              type='email'
-              name='email' // Ensure this matches {{email}}
+              type="email"
+              name="email" // Ensure this matches {{email}}
               value={form.email}
               onChange={handleChange}
               placeholder="Your email?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
               required
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
               rows={7}
-              name='message' // Ensure this matches {{message}}
+              name="message" // Ensure this matches {{message}}
               value={form.message}
               onChange={handleChange}
               placeholder="What is your message?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
               required
             />
           </label>
 
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
             {loading ? "Sending..." : "Send"}
           </button>
@@ -121,7 +125,7 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <AstroCanvas />
       </motion.div>
