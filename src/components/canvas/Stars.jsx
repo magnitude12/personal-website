38b4465ad3c15f3f2b53955a41e4faaa81ee1282
magnitude12 <113277446+magnitude12@users.noreply.github.com>
2026-06-26 17:50:38 -0500

    update canvas componenets

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,11 +1,10 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
-
+import { inSphere } from "maath/random"
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() => inSphere(new Float32Array(5000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
